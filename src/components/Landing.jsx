ab387c86cbe2662d813c708433eb355f4a82c7c1
Isaiah Thomas Galay <tomgalay@gmail.com>
2026-08-12 12:20:32 +0800

    1.39

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -18,6 +18,20 @@ export default function Landing() {
         <div className="landing-copy">
           <div className="name-plate">
             <span className="name-grid" aria-hidden="true">
+              <svg className="name-grid-svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <path className="ng-line" d="M8 0 C 8.6 30, 7.4 60, 8.4 100" />
+                <path className="ng-line" d="M26 0 C 25.3 35, 26.8 70, 26.2 100" />
+                <path className="ng-line ng-open" d="M41 0 C 42.2 15, 40.6 30, 41.9 45 M41 67 C 40.4 83, 42.1 96, 41.3 100" />
+                <path className="ng-line" d="M62 0 C 61.2 32, 63 66, 62.1 100" />
+                <path className="ng-line" d="M82 0 C 82.8 28, 81.2 64, 82.4 100" />
+                <path className="ng-line" d="M0 9 C 32 8.3, 68 9.8, 100 9.2" />
+                <path className="ng-line" d="M0 27 C 40 27.8, 72 26.2, 100 27.6" />
+                <path className="ng-line ng-open" d="M0 49 C 24 48.4, 48 49.8, 66 49.2 M80 49 C 91 49.7, 97 48.6, 100 49.2" />
+                <path className="ng-line" d="M0 70 C 30 69.3, 68 70.8, 100 70.1" />
+                <path className="ng-line" d="M0 90 C 35 90.7, 72 89.3, 100 90.4" />
+                <path className="ng-cyan" d="M50 0 C 50.5 28, 49.4 66, 50.4 100" />
+                <path className="ng-cyan" d="M0 38 C 32 37.4, 70 38.7, 100 38.2" />
+              </svg>
               <span className="name-grid-tick" style={{ top: "12%", left: "18%" }} />
               <span className="name-grid-tick" style={{ top: "74%", left: "86%" }} />
               <span className="name-grid-tick" style={{ top: "46%", left: "52%" }} />
